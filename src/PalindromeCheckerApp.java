a2142f6f3da76475f507c66d4479a4f0b0389e01
jeetheshprojects <jj4805@srmist.edu.in>
2026-03-09 09:24:20 +0530

    UC8:Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,82 @@
-class UseCase1PalindromeCheckerApp{
-    void display_welcome(){
-        System.out.println("Welcome to the Palindrome Checker Management System");
+import java.util.Scanner;
+public class PalindromeCheckerApp {
 
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-}
-class UseCase2PalindromeCheckerApp{
-    String text="madam";
-    void uc2check(){
-        char[] chararray =text.toCharArray();
-        boolean ch=false;
-        for(int i=0;i<=text.length()/2;i++){
-            for(int j=text.length()-1;j>=text.length()/2;j--){
-                if(chararray[i]==chararray[j]){
-                    ch=true;
-                }
+
+    public static Node convertToLinkedList(String str) {
+        Node head = null, tail = null;
+        for (char ch : str.toCharArray()) {
+            Node newNode = new Node(ch);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
-        System.out.println(ch);
+        return head;
     }
-}
-public class PalindromeCheckerApp {
-    public static void main(String[]args){
-        UseCase1PalindromeCheckerApp welcome=new UseCase1PalindromeCheckerApp();
-        welcome.display_welcome();
-        UseCase2PalindromeCheckerApp uc2=new UseCase2PalindromeCheckerApp();
-        uc2.uc2check();
+
+    public static Node reverse(Node head) {
+        Node prev = null, curr = head, next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow.next);
+
+        Node firstHalf = head;
+        Node tempSecond = secondHalf;
+        boolean palindrome = true;
+        while (tempSecond != null) {
+            if (firstHalf.data != tempSecond.data) {
+                palindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            tempSecond = tempSecond.next;
+        }
+
+        slow.next = reverse(secondHalf);
+
+        return palindrome;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = scanner.nextLine();
+
+        Node head = convertToLinkedList(input);
+
+        if (isPalindrome(head)) {
+            System.out.println("Is a palindrome?: True");
+        } else {
+            System.out.println("Is a palindrome?: False");
+        }
+
+        scanner.close();
     }
 }
